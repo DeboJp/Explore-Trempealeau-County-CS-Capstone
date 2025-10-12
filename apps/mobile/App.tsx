5e1a3536e5db0e37ddd9d7a30ae51cd46c6311f9
Debo Jyoti Paul <87137515+DeboJp@users.noreply.github.com>
2025-10-11 20:48:44 -0500

    APP-31 add: page navigation

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,14 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import ExploreScreen from './pages/ExploreScreen'
+import SavedScreen from './pages/SavedScreen'
+import ProfileScreen from './pages/ProfileScreen'
+
+const SocialTab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SocialTab.Navigator initialRouteName="Explore">
+        <SocialTab.Screen name="Explore" component={ExploreScreen} />
+        <SocialTab.Screen name="Saved" component={SavedScreen} />
+        <SocialTab.Screen name="Profile" component={ProfileScreen} />
+      </SocialTab.Navigator>
+    </NavigationContainer>
   );
 }
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
