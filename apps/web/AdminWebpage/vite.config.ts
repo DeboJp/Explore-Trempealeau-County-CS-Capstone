ca63028cbbd931fe8965de29c79bbb2117bd267f
Debo Jyoti Paul <87137515+DeboJp@users.noreply.github.com>
2025-12-08 12:21:02 -0600

    APP-85 ui and idea of how pg should look/features it should have.

--- apps/web/AdminWebpage/vite.config.ts
+++ apps/web/AdminWebpage/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.pbf"], // to include .pbf files in the build
 })
